8d77924b52a5b22eaac8a9ea9327923eac2e23a4
Kelsey Griffith <kelseyngriffith@gmail.com>
2018-06-15 01:39:17 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ function sayHiToGrandma(string) {
     return 'I can\'t hear you!'
   } else if(string.toUpperCase() === string) {
     return "YES INDEED!"
-  } else if('string'==='I love you, Grandma') {
+  } else {
     return "I love you, too."
   }
 }
